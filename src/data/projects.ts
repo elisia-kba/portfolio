5b2050d83973e319796188e364026079c4b38ffc
ekoumbamoung <elisia.koumba@univ-rennes1.fr>
2025-02-03 18:07:23 +0100

    amélioration présentation paragraphe

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ import { Project } from '../types';
 export const projects: Project[] = [
   {
     title: "Todo App",
-    description: "Todo List App est une application Vue.js permettant de créer, gérer et filtrer des tâches. Vous pouvez ajouter des tâches avec une description et une date, les marquer comme terminées, les modifier, ou les supprimer.",
+    description: "Todo List App est une application Vue.js permettant de créer, gérer et filtrer des tâches. Vous pouvez ajouter des tâches avec une description et une date, les marquer comme terminées, les modifier, ou les supprimer. (En cours de développement)",
     technologies: ["Vue.js", "TypeScript", "CSS"],
     imageUrl: "/todos-list-app.png",
     githubUrl: "https://github.com/elisia-kba/ToDoList",
@@ -26,7 +26,7 @@ export const projects: Project[] = [
   },
   {
     title: "Film list",
-    description: "Film List App est une petite application React qui permet de créer et gérer une liste de films. Elle contient également une page 'À propos' pour afficher des informations sur l'auteur.",
+    description: "Film List App est une petite application React qui permet de créer et gérer une liste de films. Elle contient également une page 'À propos' pour afficher des informations sur l'auteur. (En cours de développement)",
     technologies: ["React", "TypeScript", "HTML", "CSS"],
     imageUrl: "/film-app.png",
     githubUrl:"https://github.com/elisia-kba/film-list-app",
